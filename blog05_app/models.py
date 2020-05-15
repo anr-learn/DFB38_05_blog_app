46b8b66b9dfd8eea8eb07910e4b2adba5ae7542b
Art Ghost <art@ghost>
2020-05-15 17:41:12 -0500

    after migrate of blog05_app (but other apps not yet migrated ie the default django ones)

--- blog05_app/models.py
+++ blog05_app/models.py
@@ -1,3 +1,23 @@
+# DFB38_05_blog_app/blog05_app/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class BlogPost(models.Model):
+	""" A post to the blog """
+
+	postTitle = models.CharField(max_length=200)
+	# many-to-one (many posts per each author)
+	# (Each post has only 1 author)
+	postAuthor = models.ForeignKey(
+					"auth.User",
+					on_delete=models.CASCADE)
+	postBody = models.TextField()
+
+	def __str__(self):
+		return ("%s[%s]" % 
+			(self.__class__.__name__,
+			 self.postTitle))
+
+
+### end ###
