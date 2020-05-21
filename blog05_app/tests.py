4db081cceb36be319ed22f83b9a6770e9541fc3c
Art Ghost <art@ghost>
2020-05-21 18:27:53 -0500

    added more unit tests

--- blog05_app/tests.py
+++ blog05_app/tests.py
@@ -40,8 +40,25 @@ class BlogTests(TestCase):
 		self.assertContains(response, "This is the blog body")
 		self.assertTemplateUsed(response, "home.html")
 
+	def test_good_post_detail_view(self):
+		""" 'good' detail view """
+		response = self.client.get("/post/1/")
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "This is the blog body")
+		self.assertTemplateUsed(response, "post_detail.html")
+		#print(f"@@@ @@@ {dir(response)}")
+		#print(f"@@@ @@@ template name: {response.template_name}")
 
-
+	def test_nosuch_detail(self):
+		""" try fetching a non-existent BlogPost obj """
+		response = self.client.get("/post/2/")
+		self.assertEqual(response.status_code, 404)
+		# We got an error before trying to use a template,
+		# so no template was accessed
+		###print(f"@@@ {response}")
+		###print(f"@@@ @@@ template name: {response.template_name}")
+		###self.assertTemplateUsed(response, None)
+		self.assertFalse(hasattr(response, "template_name"))
 
 
 ### end ###
